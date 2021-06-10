913ffbf6e290ad07b2df866f679c3042119fa3d7
vbolinches <vbolinches@alfatecsistemas.es>
2021-06-10 18:32:08 +0200

    refactors & tests passed

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,16 +1,44 @@
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+const FIZZBUZZ = FIZZ + BUZZ;
+const LN = "\n";
 
+/**
+ * Function generic mod
+ * @param i 
+ * @param op 
+ * @returns 
+ */
+function mod(i : number, op : number) : boolean{
+    return (i % op) == 0
+}
+
+/**
+ * Function mod 3 & 5
+ * @param i 
+ * @returns 
+ */
+function mod_3_5(i: number) : boolean {
+    return mod(i,3) && mod(i, 5);
+}
+
+/**
+ * Input Method
+ * @param input 
+ * @returns 
+ */
 export function fizzBuzz(input : Number) : String {
    var res = "";
 
-   for(var i = 1; i<=input; i++){
-       if (i % 3 == 0 && i % 5 == 0)
-        res += "FizzBuzz" + "\n";
-       else if( i % 3 == 0)
-        res += "Fizz" + "\n";
-       else if( i % 5 == 0)
-        res += "Buzz" + "\n";
+   for(var i = 1; i <= input; i++){
+       if (mod_3_5(i))
+        res += FIZZBUZZ + LN;
+       else if( mod(i,3) )
+        res += FIZZ + LN;
+       else if( mod(i,5) )
+        res += BUZZ + LN;
        else 
-        res += i +"\n";
+        res += i + LN;
    }
 
    return res;
