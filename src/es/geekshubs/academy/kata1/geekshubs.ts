6b4c9c297599189dd41a08aad554ff6df2ef7f22
vbolinches <vbolinches@alfatecsistemas.es>
2021-06-10 18:14:02 +0200

    test 3 passed

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -5,7 +5,9 @@ export function fizzBuzz(input : Number) : String {
    for(var i = 1; i<=input; i++){
        if( i % 3 == 0)
         res += "Fizz" + "\n";
-      else 
+       else if( i % 5 == 0)
+        res += "Buzz" + "\n";
+       else 
         res += i +"\n";
    }
 
