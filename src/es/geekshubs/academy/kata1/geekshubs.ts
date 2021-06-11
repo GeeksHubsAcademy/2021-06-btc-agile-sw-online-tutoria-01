8691067bb51dd23440778516229530cca3a446d2
japc <japc.grafico@gmail.com>
2021-06-11 17:06:03 +0200

    Refractor extract values and methods 1

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -1,18 +1,35 @@
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+const FIZZBUZZ = FIZZ + BUZZ;
+const LN = "\n"
 
-export function fizzBuzz(input : Number) : String {
+function mod_3(input: number): boolean {
+   return (input % 3 === 0)
+}
+
+function mod_5(input: number): boolean {
+   return (input % 5 === 0)
+}
+
+function mod_3_5(input: number): boolean {
+   return (mod_3(input) && mod_5(input))
+}
+
+
+
+export function fizzBuzz(input : number) : String {
     let res = "";
 
     for (let i = 1; i <= input; i++) {
-        if ((i % 3 === 0) && (i % 5 === 0))
-            res += "FizzBuzz" + "\n"
-        else if (i % 3 === 0)
-            res += "Fizz" + "\n"
-        else if (i % 5 === 0)
-            res += "Buzz" + "\n"
+        if (mod_3_5(i))
+            res += FIZZBUZZ + LN
+        else if (mod_3(i))
+            res += FIZZ + LN
+        else if (mod_5(i))
+            res += BUZZ + LN
         else
-            res += i + "\n";
+            res += i + LN;
     }
 
     return res;
 };
-
