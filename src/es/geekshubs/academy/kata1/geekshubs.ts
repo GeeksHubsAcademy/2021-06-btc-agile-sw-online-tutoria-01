8bbd8b30af778758fc0e2f5e7f7c7e52fc87f0b7
japc <japc.grafico@gmail.com>
2021-06-11 16:21:46 +0200

    Test 2 passed

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -3,7 +3,10 @@ export function fizzBuzz(input : Number) : String {
     let res = "";
 
     for (let i = 1; i <= input; i++) {
-        res += i + "\n";
+        if (i % 3 === 0)
+            res += "Fizz" + "\n"
+        else
+            res += i + "\n";
     }
 
     return res;
