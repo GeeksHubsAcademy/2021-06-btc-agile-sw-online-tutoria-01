e31e153dc03d402be67de91958b31cfb9a1f129f
japc <japc.grafico@gmail.com>
2021-06-11 16:44:30 +0200

    test all passed

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -3,7 +3,9 @@ export function fizzBuzz(input : Number) : String {
     let res = "";
 
     for (let i = 1; i <= input; i++) {
-        if (i % 3 === 0)
+        if ((i % 3 === 0) && (i % 5 === 0))
+            res += "FizzBuzz" + "\n"
+        else if (i % 3 === 0)
             res += "Fizz" + "\n"
         else if (i % 5 === 0)
             res += "Buzz" + "\n"
